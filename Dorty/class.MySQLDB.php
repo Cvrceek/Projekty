050601c823bea0f00c8bdb59cff9cdcc88a91af3
Martin Švrček <martinsvrcek94@gmail.com>
2018-04-11 18:21:21 +0200

    upraven login do databaze

--- Dorty/class.MySQLDB.php
+++ Dorty/class.MySQLDB.php
@@ -43,9 +43,9 @@
 
 error_reporting(0);
 
-define( 'DB_HOST', 'localhost' ); // set database host
-define( 'DB_USER', 'root' ); // set database user
-define( 'DB_PASS', '' ); // set database password
+define( 'DB_HOST', 'c093um.forpsi.com:3306' ); // set database host
+define( 'DB_USER', 'f103094' ); // set database user
+define( 'DB_PASS', 'mquNS25' ); // set database password
 define( 'DB_NAME', 'dorty' ); // set database name
 define( 'SEND_ERRORS_TO', '' ); //set email notification email address
 define( 'DISPLAY_DEBUG', true ); //display db errors?
